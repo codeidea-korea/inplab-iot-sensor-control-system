5780bfad8222a30462a7933cb78ce6857a6d2ea2
h0ngg0m <jaehongbyeon98@gmail.com>
2025-02-14 16:31:37 +0900

    :recycle: 코드 정리

--- src/main/java/com/safeone/dashboard/scheduler/AlarmScheduler.java
+++ src/main/java/com/safeone/dashboard/scheduler/AlarmScheduler.java
@@ -30,20 +30,11 @@ public class AlarmScheduler {
     private final AlarmSchedulerService alarmSchedulerService;
     private final AlarmListService alarmListService;
     private final AssetListService assetListService;
-    private final static SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
     private static String lastCollectTime = null;
-
     private final static long DELAY_MS = 60 * 1000; // 60초
-
-
-    @Autowired
-    Environment env;
-
-    @Autowired
-    SmsService smsService;
-
-    @Autowired
-    SmsAlarmService smsAlarmService;
+    private final Environment env;
+    private final SmsService smsService;
+    private final SmsAlarmService smsAlarmService;
 
     /**
      * 계측기 통신 장애 관련 알람 스케쥴러(1분)
@@ -63,14 +54,12 @@ public class AlarmScheduler {
             for (Map param : result) {
                 Map map = new HashMap();
                 map.put("asset_id", param.get("asset_id").toString());
-//                map.put("alarm_desc", param.get("description").toString());
                 map.put("status_hid", "2");
-//                System.out.println(map);
                 assetListService.update(map);
+
                 // 알람설정에서 사용여부에 따라 알람 insert
                 if (param.get("use_flag").equals("Y")) {
                     alarmListService.create(param);
-//                    sendAlarmSMS(param, "통신 장애");
                 }
             }
         } catch (Exception e) {
@@ -103,19 +92,16 @@ public class AlarmScheduler {
             m.put("asset_id", assetListDto.getAsset_id());
 
             if (!url.contains("rtsp://")) {
-//                m.put("alarm_desc", "Invalid rtsp url - " + url);
                 if (m.get("use_flag").equals("Y")) {
                     alarmListService.create(m);
                 }
                 assetListService.update(param);
                 continue;
             }
-
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(url);
 
             if (!matcher.find()) {
-//                m.put("alarm_desc", "URL pattern mismatch - " + url);
                 if (m.get("use_flag").equals("Y")) {
                     alarmListService.create(m);
                 }
@@ -127,16 +113,11 @@ public class AlarmScheduler {
             int port = Integer.parseInt(matcher.group(2));
 
             try (Socket socket = new Socket(ipAddress, port)) {
-                // Connection successful
                 param.put("status_hid", "1");
             } catch (IOException e) {
-                // Connection failed
-//                m.put("alarm_desc", "Connection failed to CCTV at " + ipAddress + ":" + port);
                 if (m.get("use_flag").equals("Y")) {
                     alarmListService.create(m);
-//                    sendAlarmSMS(m, "통신 장애");
                 }
-//                e.printStackTrace();
             }
             assetListService.update(param);
         }
@@ -152,25 +133,25 @@ public class AlarmScheduler {
                 continue;
             }
 
-            String mms = "[" + title + " 알람]\n" + 
-                "현장명 : " + alarmInfo.get("area_name").toString() + " / " + alarmInfo.get("zone_name").toString() + "\n" + 
-                "현장주소 : " + alarmInfo.get("address").toString() + "\n\n";
+            String mms = "[" + title + " 알람]\n" +
+                    "현장명 : " + alarmInfo.get("area_name").toString() + " / " + alarmInfo.get("zone_name").toString() + "\n" +
+                    "현장주소 : " + alarmInfo.get("address").toString() + "\n\n";
 
             int smsCount = smsAlarmService.selectSmsCountPerDay(target.getPhone());
-            
+
             mms += "[24시간내 전송된 회차]\n회차 : " + (smsCount + 1) + "\n\n";
 
             if (title.equals("통신 장애")) {
-                mms += "[현장 요약]\n" + 
-                    "알람레벨 : " + alarmInfo.get("risk_name").toString() + "\n" + 
-                    "센서종류 : " + alarmInfo.get("asset_kind_name").toString() + "\n" + 
-                    "센서명 : " + alarmInfo.get("asset_name").toString() + "\n\n";
+                mms += "[현장 요약]\n" +
+                        "알람레벨 : " + alarmInfo.get("risk_name").toString() + "\n" +
+                        "센서종류 : " + alarmInfo.get("asset_kind_name").toString() + "\n" +
+                        "센서명 : " + alarmInfo.get("asset_name").toString() + "\n\n";
             } else {
-                mms += "[현장 요약]\n" + 
-                    "알람레벨 : " + alarmInfo.get("risk_name").toString() + "\n" + 
-                    "센서종류 : " + alarmInfo.get("asset_kind_name").toString() + "\n" + 
-                    "센서명 : " + param.get("channel_name").toString() + "\n" + 
-                    "센서값 : " + param.get("real_value").toString() + "\n";
+                mms += "[현장 요약]\n" +
+                        "알람레벨 : " + alarmInfo.get("risk_name").toString() + "\n" +
+                        "센서종류 : " + alarmInfo.get("asset_kind_name").toString() + "\n" +
+                        "센서명 : " + param.get("channel_name").toString() + "\n" +
+                        "센서값 : " + param.get("real_value").toString() + "\n";
 
                 Map rainGuage = smsAlarmService.selectRainGaugeInfo(param);
 
@@ -181,16 +162,13 @@ public class AlarmScheduler {
                 }
             }
 
-            mms += "[시공업체정보]\n" + 
-                "시공사 : " + alarmInfo.get("constructor").toString() + "\n" +
-                "담당자명 : 최덕만 부장\n" + 
-                "연락처 : 010-3164-2913";
+            mms += "[시공업체정보]\n" +
+                    "시공사 : " + alarmInfo.get("constructor").toString() + "\n" +
+                    "담당자명 : 최덕만 부장\n" +
+                    "연락처 : 010-3164-2913";
 
-            smsService.send(target.getPhone().toString().replace("-", ""), mms);
+            smsService.send(target.getPhone().replace("-", ""), mms);
         }
-
-//        System.out.println(param);
-//        System.out.println(alarmInfo);
     }
 
     @Scheduled(fixedDelay = DELAY_MS)
@@ -205,13 +183,7 @@ public class AlarmScheduler {
 
         List<Map> alarmList = alarmSchedulerService.selectThresholdAlarm(param);
 
-        // {raw_value=31827, num=0662, asset_kind_id=2, mod_date=2023-11-20 10:42:51.693302, asset_id=6, area_id=0, type=2, sensor_id=053, 
-        //  zone_id=1, collect_date=2023/11/22/16:53:32, install_date=2023-09-13 00:00:00.0, seq=1, real_value=0.02, range_value5=150, mod_user=test_admin, 
-        //  calc_value=0.94, range_value3=50, range_value4=100, device_id=, range_value1=-10, range_value2=10, _cdate=2023-11-22T16:48:45.404, 
-        //  reg_date=2023-09-13 12:15:34.15349, name=TTW-02-02, x=128.3408891746636, y=36.12744754695781, channel_id=12, use_flag=Y, status=2}
         for (Map alarm : alarmList) {
-            // System.out.println(alarm);
-
             lastCollectTime = alarm.get("collect_date").toString();
 
             // description 생성
@@ -231,28 +203,13 @@ public class AlarmScheduler {
                     alarm.put("alarm_kind_id", "10");
                 }
                 alarm.put("type", "2");
-                alarm.put("name", alarm.get("asset_kind_name").toString() + " " +  alarm.get("asset_name").toString());
-                alarm.put("description", alarm.get("channel_name").toString() + " 센서값 " +  alarm.get("real_value").toString());
+                alarm.put("name", alarm.get("asset_kind_name").toString() + " " + alarm.get("asset_name").toString());
+                alarm.put("description", alarm.get("channel_name").toString() + " 센서값 " + alarm.get("real_value").toString());
 
                 alarmListService.create(alarm);
                 sendAlarmSMS(alarm, "임계치");
             }
         }
     }
-
-    /**
-     * 알람 현황 표시 - 1분마다 지난 알람 카운트 안되게 view_flag N으로 update
-     */
-//    @Scheduled(fixedDelay = DELAY_MS)
-//    public void updateViewFlagScheduler() {
-//        if (env.getProperty("dev").equalsIgnoreCase("true"))
-//            return;
-//
-//        try {
-//            alarmSchedulerService.updateViewFlag();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
 
